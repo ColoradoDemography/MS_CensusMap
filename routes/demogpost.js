19cf2b1917eb6fdd7d9839e9b1a1b2286f9533c8
TBleess <todd.bleess@state.co.us>
2026-02-02 15:50:41 -0700

    Update default database from acs1014 to acs2024

--- routes/demogpost.js
+++ routes/demogpost.js
@@ -383,10 +383,10 @@ function array_unshift(array) {
   
  //potential single select
 var type = req.body.type || 'json';
-var db = req.body.db || 'acs1014';
+var db = req.body.db || 'acs2024';
 //set default for schema if it is missing
 var schema = req.body.schema ||  function(){
-  if(db==='acs1923' || db==='acs1822' || db==='acs1721' || db==='acs1620' || db==='acs1519' || db==='acs1418' || db==='acs1317' || db==='acs1216' || db==='acs1115' || db==='acs1014' || db==='acs0913' || db==='acs0812' || db==='acs0711' || db==='acs0610' || db==='c2010' ){return 'data';}
+  if((db==='acs2024' || db==='acs1923' || db==='acs1822' || db==='acs1721' || db==='acs1620' || db==='acs1519' || db==='acs1418' || db==='acs1317' || db==='acs1216' || db==='acs1115' || db==='acs1014' || db==='acs0913' || db==='acs0812' || db==='acs0711' || db==='acs0610' || db==='c2010' ){return 'data';}
   if(db==='c2000' || db==='c1990' || db==='c1980'){return 'sf1';}  
   return '';  //no valid database - will deal with later 
 };
@@ -402,7 +402,7 @@ var schema = req.body.schema ||  function(){
 
 //if database is acs, check to see if moe option is flagged
 var moe='no';
-if(db==='acs1923' || db==='acs1822' || db==='acs1721' || db==='acs1620' || db==='acs1519' || db==='acs1418' || db==='acs1317' || db=='acs0610' || db=='acs0711' || db=='acs0812' || db=='acs0913' || db=='acs1014' || db=='acs1115' || db=='acs1216'){
+if((db==='acs2024' || db==='acs1923' || db==='acs1822' || db==='acs1721' || db==='acs1620' || db==='acs1519' || db==='acs1418' || db==='acs1317' || db=='acs0610' || db=='acs0711' || db=='acs0812' || db=='acs0913' || db=='acs1014' || db=='acs1115' || db=='acs1216'){
   if (req.body.moe){
     moe=req.body.moe;
   }
@@ -456,7 +456,7 @@ if(db==='acs1923' || db==='acs1822' || db==='acs1721' || db==='acs1620' || db===
   
   
   //validate database selected
-    if(db!=='c1980' && db!=='c1990' && db!=='c2000' && db!=='c2010' && db!=='acs0610' && db!=='acs0711' && db!=='acs0812' && db!=='acs0913' && db!=='acs1014' && db!=='acs1115' && db!=='acs1216' && db!=='acs1317' && db!=='acs1418' && db!=='acs1519' && db!=='acs1620' && db!=='acs1721' && db!=='acs1822' && db!=='acs1923'){
+    if(db!=='c1980' && db!=='c1990' && db!=='c2000' && db!=='c2010' && db!=='acs0610' && db!=='acs0711' && db!=='acs0812' && db!=='acs0913' && db!=='acs1014' && db!=='acs1115' && db!=='acs1216' && db!=='acs1317' && db!=='acs1418' && db!=='acs1519' && db!=='acs1620' && db!=='acs1721' && db!=='acs1822' && db!=='acs1923' && db!=='acs2024'){
       errorarray.push('Your database choice `' + db + '` is not valid.');
       db="";
       return 'error'; //goto a;
